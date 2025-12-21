8ddd860b2b43d66331e979df2b4213488f13fc5b
svinoczar <alexsupper75@gmail.com>
2025-12-21 09:53:33 +0300

    feat: add lang detection model

---
+++ src/services/internal/lang_analyzer_model.py
@@ -0,0 +1,64 @@
+from transformers import AutoTokenizer
+from onnxruntime import InferenceSession
+import numpy as np
+import os
+import json
+
+
+class LanguageDetector:
+    def __init__(self):
+        base_dir = os.path.dirname(__file__)
+        model_dir = os.path.join(
+            base_dir, "..", "..", "models", "lang_detect"
+        )
+
+        self.session = InferenceSession(
+            os.path.join(model_dir, "model.onnx"),
+            providers=["CPUExecutionProvider"]
+        )
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "philomath-1209/programming-language-identification"
+        )
+
+        with open(os.path.join(model_dir, "config.json"), "r", encoding="utf-8") as f:
+            cfg = json.load(f)
+            self.id2label = {int(k): v for k, v in cfg["id2label"].items()}
+
+    def detect(self, code: str, threshold: float = 0.7):
+        inputs = self.tokenizer(
+            code,
+            return_tensors="np",
+            truncation=True,
+            max_length=512
+        )
+
+        ort_inputs = {
+            "input_ids": inputs["input_ids"],
+            "attention_mask": inputs["attention_mask"]
+        }
+
+        logits = self.session.run(None, ort_inputs)[0][0]
+        probs = self._softmax(logits)
+
+        idx = int(np.argmax(probs))
+        confidence = float(probs[idx])
+
+        if confidence < threshold:
+            return "Unknown", confidence
+
+        return self.id2label[idx], confidence
+
+    @staticmethod
+    def _softmax(x):
+        e = np.exp(x - np.max(x))
+        return e / e.sum()
+
+detector = LanguageDetector()
+
+# clean_code = preprocess(code)
+# print(clean_code)
+
+lang, conf = detector.detect(code)
+
+print(lang, conf)
